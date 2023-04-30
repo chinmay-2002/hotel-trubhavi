8504f8b16349d27ac2b4a203e9ff04de92d3068d
deep <deepaidasani1435@gmail.com>
2023-05-01 01:20:39 +0530

    hii

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ include('db.php');
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<h1><a class="navbar-brand" href="index.php">SUN <span>RISE</span><p class="logo_w3l_agile_caption">Your Dreamy Resort</p></a></h1>
+					<h1><a class="navbar-brand" href="index.php">HOTEL <span>TRUBHAVI</span><p class="logo_w3l_agile_caption">Your Dreamy Resort</p></a></h1>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
